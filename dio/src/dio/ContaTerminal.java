cd37740c7b7f0e13d794d4f2f0e8767ae26c84c5
Eduardo <157758613+EduardoUsername@users.noreply.github.com>
2024-05-15 18:08:42 -0300

    Add files via upload

--- dio/src/dio/ContaTerminal.java
+++ dio/src/dio/ContaTerminal.java
@@ -1,5 +1,31 @@
 package dio;
+import java.util.Scanner;
 
 public class ContaTerminal {
+	public static void main(String args[]) throws Exception {
+		int numero;
+		String agencia;
+		String nomeCliente;
+		double saldo;
+		Scanner in = new Scanner(System.in);
+		System.out.println("Por favor, insira seu nome: ");
+		nomeCliente = in.nextLine();
+		System.out.println("Por favor, insira o numero da sua conta: ");
+		while(!in.hasNextInt()) {
+			System.out.println("Por favor, insira um número válido.");
+			in.next();
+		}
+		numero = in.nextInt();
+		in.nextLine();
+		System.out.println("Por favor, insira o seu numero de agência: ");
+		agencia = in.nextLine();
+		System.out.println("Por favor, insira seu saldo: ");
+		while(!in.hasNextDouble()) {
+			System.out.println("Por favor, insira um saldo válido.");
+			in.next();
+		}
+		saldo = in.nextDouble();
+		System.out.println("Olá " + nomeCliente + " obrigado por criar uma conta em nosso banco, sua agência é " + agencia + " conta "+ numero + " e seu saldo " + saldo + " já está disponível para saque.");
+	}
 
 }
